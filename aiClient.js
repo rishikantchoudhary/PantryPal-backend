03d75aa83782937f6ad452845aae6b463cc1753b
Rishi <rishikantchoudhary951@gmail.com>
2025-09-12 13:36:00 +0530

    Initial commit: PantryPal backend

---
+++ aiClient.js
@@ -0,0 +1,63 @@
+import { GoogleGenAI, Type } from '@google/genai'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+})
+
+async function generateRecipe(ingredients) {
+  try {
+    const model = 'gemini-2.5-flash-lite'
+    const prompt = `You are an expert recipe recommendation assistant. Suggest three dishes using the following ingredients I have : ${ingredients}. Return 'recipeName' is name of the recipe, 'servings' is number of servings, 'estimatedTime' is estimated time in minutes, 'ingredientsYouHave' is only the ingredients I mentioned with required quantity, 'missingIngredients' is any additional ingredients I might need to make this dish with quantity, 'steps' is steps to make this dish in around 8-10 steps.`
+
+    const response = await ai.models.generateContent({
+      model: model,
+      contents: prompt,
+      config: {
+        responseMimeType: 'application/json',
+        responseSchema: {
+          type: Type.ARRAY,
+          items: {
+            type: Type.OBJECT,
+            properties: {
+              recipeName: { type: Type.STRING },
+              servings: { type: Type.NUMBER },
+              estimatedTime: { type: Type.STRING },
+              ingredientsYouHave: {
+                type: Type.ARRAY,
+                items: { type: Type.STRING },
+              },
+              missingIngredients: {
+                type: Type.ARRAY,
+                items: { type: Type.STRING },
+              },
+              steps: {
+                type: Type.ARRAY,
+                items: { type: Type.STRING },
+              },
+            },
+            propertyOrdering: [
+              'recipeName',
+              'servings',
+              'estimatedTime',
+              'ingredientsYouHave',
+              'missingIngredients',
+              'steps',
+            ],
+          },
+        },
+      },
+    })
+
+    const rawText = response.candidates[0].content.parts[0].text
+
+    return JSON.parse(rawText)
+  } catch (error) {
+    console.log('Error generating the recipe: ', error)
+    throw error
+  }
+}
+
+export default generateRecipe
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import generateRecipe from './aiClient.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+// Middleware
+app.use(cors())
+app.use(express.json()) // allows JSON in request body
+
+// To generate recipes dynamically from gemini api
+app.post('/recipes', async (req, res) => {
+  try {
+    const { ingredients } = req.body
+
+    const recipe = await generateRecipe(ingredients)
+
+    res.json({
+      message: 'Recipes generated successfully',
+      recipe,
+    })
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to generate recipe' })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`)
+})
