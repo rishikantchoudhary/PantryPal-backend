22df21624910288c356b8be4425a1b01a332cac0
Rishi <rishikantchoudhary951@gmail.com>
2025-09-14 13:24:48 +0530

    Updated Cors.

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+  })
+)
 app.use(express.json()) // allows JSON in request body
 
 // To generate recipes dynamically from gemini api
